a287f4b31eabb95d58a2b3733c390825817465b6
Krylovsentry <krylovsentry@yandex.ru>
2017-08-31 04:27:40 +0700

    initial commit

---
+++ morningbot.py
@@ -0,0 +1,11 @@
+import datetime
+from BotHandler import BotHandler
+
+def main():
+    # identifier of the first update to be returned.
+    new_offset = None
+    morningBot = BotHandler(token)
+
+
+if __name__ == '__main__':
+    main()
